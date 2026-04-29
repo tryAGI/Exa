8df227eb8a9466391709304b59c0626ded17c93d
Konstantin S. <havendv@gmail.com>
2026-04-29 06:13:24 +0400

    feat: Updated OpenAPI spec (#26)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Exa/Generated/Exa.ExaClient.ResearchControllerV0GetResearchTask.g.cs
+++ src/libs/Exa/Generated/Exa.ExaClient.ResearchControllerV0GetResearchTask.g.cs
@@ -124,7 +124,7 @@ namespace Exa
                 PrepareResearchControllerV0GetResearchTaskRequest(
                     httpClient: HttpClient,
                     httpRequestMessage: __httpRequest,
-                    id: id);
+                    id: id!);
 
                 return __httpRequest;
             }

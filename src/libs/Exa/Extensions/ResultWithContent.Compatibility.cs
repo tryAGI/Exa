e2eb0650a04f2f5c98d81a63f73028ca2089b28f
Konstantin S. <havendv@gmail.com>
2026-05-07 16:33:49 +0400

    fix: restore local SDK build

---
+++ src/libs/Exa/Extensions/ResultWithContent.Compatibility.cs
@@ -0,0 +1,11 @@
+#nullable enable
+#pragma warning disable CS1591
+
+namespace Exa;
+
+public readonly partial struct ResultWithContent
+{
+    public Result? Value1 => Result;
+
+    public ResultWithContentVariant2? Value2 => ResultWithContentVariant2;
+}
